38c161f7721caf78a93ec6f19c82fbb6a3be2c27
levk1s <oleksandrvolosuk2404@outlook.com>
2025-11-02 17:05:58 +0100

    Added color palets and fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,15 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     modules: ['@nuxt/image', '@nuxtjs/tailwindcss'],
+    
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;500;600;700&family=Manrope:wght@400&display=swap'
+                }
+            ]
+        }
+    }
 });
