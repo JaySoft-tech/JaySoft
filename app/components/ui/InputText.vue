63f1b02e2f79778ad173d28a20640e9c66d2a2b4
ExLyD1 <sashaahnij@gmail.com>
2025-11-25 21:26:32 +0200

    [+] created InputText component & [+] created ContactForm

---
+++ app/components/ui/InputText.vue
@@ -0,0 +1,35 @@
+<template>
+    <input
+        ref="inputRef"
+        type="text"
+        :name="name"
+        :placeholder="placeholder"
+        v-model="model"
+        :disabled="disabled"
+        :class="[
+            'bg-[#FFFFFF] h-full rounded-[14px] w-full py-[10px] md:py-[23px] px-[17px] md:px-[36px] text-[#777777] text-[11px]/[100%] md:text-[22px]/[100%] font-light outline-none font-unbounded',
+            sizeClass,
+        ]"
+    />
+</template>
+
+<script setup lang="ts">
+interface InputProps {
+    name: string;
+    placeholder?: string;
+    disabled?: boolean;
+    size: 'sm' | 'lg';
+}
+
+const props = defineProps<InputProps>();
+const model = defineModel<string>();
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const sizeClass = computed(() => {
+    return props.size === 'lg'
+        ? 'max-h-[100px] md:max-h-[146px]'
+        : 'max-h-[40px] md:max-h-[73px]';
+});
+</script>
+
+<style scoped></style>
---
+++ app/features/contact-form/ui/ContactForm.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="w-full max-w-[1240px] place-self-center">
+        <!-- heading text -->
+        <h2 class="text-section-heading-lg font-unbounded">КОНТАКТИ</h2>
+
+        <!-- form container -->
+        <div
+            class="flex items-center justify-center mt-[46px] py-[45px] rounded-[50px] bg-[#D9F3EF]"
+        >
+            <!-- content -->
+            <div
+                class="max-w-[743px] w-full flex flex-col items-center justify-center gap-[37px]"
+            >
+                <p
+                    class="max-w-[605px] text-[29px]/[100%] font-light text-center text-[#000000] font-unbounded"
+                >
+                    Зв'яжіться з нами! <br />
+                    До Вашої мети — всього один клік!
+                </p>
+
+                <div class="flex flex-col gap-[23px] w-full">
+                    <InputText
+                        v-for="item in formInputs"
+                        :key="item.id"
+                        :name="item.name"
+                        :placeholder="item.placeholder"
+                        :size="item.size"
+                        v-model="form[item.modelKey]"
+                    />
+                </div>
+
+                <Button
+                    @click="handleClick"
+                    label="Надіслати"
+                    variant="primary"
+                    icon="send"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import InputText from '~/components/ui/InputText.vue';
+import Button from '~/components/ui/Button.vue';
+
+interface IForm {
+    name: string;
+    phone: string;
+    email: string;
+    text: string;
+}
+
+interface IInput {
+    id: number;
+    name: string;
+    placeholder?: string;
+    size: 'sm' | 'lg';
+    modelKey: keyof IForm;
+}
+
+const form = reactive<IForm>({
+    name: '',
+    phone: '',
+    email: '',
+    text: '',
+});
+
+const formInputs = ref<IInput[]>([
+    {
+        id: 1,
+        name: 'name',
+        placeholder: "Ім'я",
+        size: 'sm',
+        modelKey: 'name',
+    },
+    {
+        id: 2,
+        name: 'phone',
+        placeholder: 'Номер телефону*',
+        size: 'sm',
+        modelKey: 'phone',
+    },
+    {
+        id: 3,
+        name: 'email',
+        placeholder: 'Email*',
+        size: 'sm',
+        modelKey: 'email',
+    },
+    {
+        id: 4,
+        name: 'message',
+        placeholder: 'Повідомлення',
+        size: 'lg',
+        modelKey: 'text',
+    },
+]);
+
+const handleClick = () => {
+    console.log('Form data is ==> ', form);
+};
+</script>
+
+<style scoped></style>
