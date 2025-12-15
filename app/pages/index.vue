2b9f66d1d53d5e2f272e4c077aee70fd332e7235
levk1s <oleksandrvolosuk2404@outlook.com>
2025-12-15 19:28:42 +0100

    fix

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,42 +5,12 @@
         <HeroSection />
 
         <AboutSection />
-
-        <ProjectSection />
-
-        <ServicesSection />
-
-        <ContactForm />
     </div>
 </template>
 
 <script setup lang="ts">
 import HeroSection from '~/features/home/ui/HeroSection.vue';
 import AboutSection from '~/features/home/ui/AboutSection.vue';
-import ContactForm from '~/features/contact-form/ui/ContactForm.vue';
-import ServicesSection from '~/features/home/ui/ServicesSection.vue';
-import ProjectSection from '~/features/home/ui/ProjectSection.vue';
-
-const handleClick = () => {
-    console.log('Кнопка натиснута!');
-};
-
-const handleCardButtonClick = () => {
-    console.log('Кнопка картки натиснута!');
-};
-
-const activeFilter = ref('landing');
-
-const filters = [
-    { id: 'landing', label: 'Лендінг' },
-    { id: 'multipage', label: 'Багатосторінковий' },
-    { id: 'ecommerce', label: 'Інтернет-магазин' },
-];
-
-const setFilter = (filterId: string) => {
-    activeFilter.value = filterId;
-    console.log('Активний фільтр:', filterId);
-};
 </script>
 
 <style scoped></style>
